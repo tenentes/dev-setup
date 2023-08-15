c583963401a6d4dc465e3f7a618c5f90594dcf06
tenentes <tenentes@gmail.com>
2023-08-15 16:16:04 -0400

    Added version check script

---
+++ check_versions.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+echo "Python Version:"
+python --version
+python3 --version
+
+echo "Visual Studio Code Version:"
+code --version
+
+echo "Git Version:"
+git --version
+
+echo "Homebrew Version:"
+brew --version
+
+echo "Anaconda Version:"
+conda --version
+
+echo "Docker Version:"
+docker --version
+
+echo "Node.js Version:"
+node --version
+
+echo "npm Version:"
+npm --version
+
