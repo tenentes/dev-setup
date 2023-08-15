079a32ecee567efc0b1c2b30f9ec822ad8b7c479
tenentes <tenentes@gmail.com>
2023-08-15 17:07:30 -0400

    Updated the setup scripts and made necessary changes

---
+++ find_applications.sh
@@ -0,0 +1,18 @@
+echo "Searching for Python..."
+find / -name "python" -type f -executable 2>/dev/null
+
+echo "Searching for Visual Studio Code..."
+find / -name "code" -type f -executable 2>/dev/null
+
+echo "Searching for Homebrew..."
+find / -name "brew" -type f -executable 2>/dev/null
+
+echo "Searching for Anaconda..."
+find / -name "conda" -type f -executable 2>/dev/null
+
+echo "Searching for Node.js..."
+find / -name "node" -type f -executable 2>/dev/null
+
+echo "Searching for npm..."
+find / -name "npm" -type f -executable 2>/dev/null
+
